d399e6fae6b8ffa9fcf473bccb48f9f683849d48
72464_Mayuri <mayuripatade08@gmail.com>
2023-06-15 18:47:08 +0530

    todo

--- src/App.js
+++ src/App.js
@@ -3,55 +3,37 @@ import { useRef, useState } from "react";
 function App() {
   return (
     <>
-      <h1>Stateful List</h1>
-      <ListDemo />
+      <h1>My Todo</h1>
+      <MyTodo />
     </>
   );
 }
 
-function ListDemo() {
-  let inputRef = useRef();
-  let [list, setList] = useState(["delhi"]);
+function MyTodo() {
+  //let todo = {task: ""};
+  let [todo, setTodo] = useState({ task: "" });
 
-  let addItemAction = () => {
-    let inputValue = inputRef.current.value;
-    let newList = [inputValue, ...list];
-    setList(newList);
+  let handleChangeTaskAction = (e) => {
+    console.log(e.target);
 
-    inputRef.current.value = "";
+    let newTodo = { ...todo, task: e.target.value };
+    setTodo(newTodo);
+  };
+
+  let addTodoAction = () => {
+    alert(todo.task);
   };
 
   return (
     <>
       <input
+        className="form-control"
         type="text"
-        id="id1"
-        ref={inputRef}
-        placeholder="Enter user input..."
+        placeholder="Enter Task"
+        value={todo.task}
+        onChange={handleChangeTaskAction}
       />
-      <input type="button" value="Add New Item" onClick={addItemAction} />
-
-      {list.map((item) => (
-        <MessageDemo message={item} />
-      ))}
-    </>
-  );
-}
-
-function MessageDemo({ message }) {
-  return (
-    <>
-      <h1>Hello {message}</h1>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus,
-        dolorem quas sequi tenetur quo odit cupiditate assumenda voluptas
-        quibusdam blanditiis voluptatem quis mollitia eaque molestiae animi,
-        provident eligendi facilis incidunt.
-      </p>
-      <div>
-        <input type="button" value="&#128077;" />
-        <input type="button" value="&#128078;" />
-      </div>
+      <input type="button" value="Add Todo" onClick={addTodoAction} />
     </>
   );
 }
