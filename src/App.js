8bb7d77049667c3a2eb7df0ea7ca3a3ebd0ba6a6
72464_Mayuri <mayuripatade08@gmail.com>
2023-06-15 14:54:02 +0530

    b3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <>
-    <h1>Props Demo</h1>
-    <MessageDemo  username="mayuri" email="m@gmail.com"/>
-    <MessageDemo  username="cdac" email="c@gmail.com"/>
-    <MessageDemo  username="Student" email="s@gmail.com"/>
+      <h1>Props Demo</h1>
+      <MessageDemo username="mayuri" email="m@gmail.com" />
+      <MessageDemo username="cdac" email="c@gmail.com" />
+      <MessageDemo username="Student" email="s@gmail.com" />
     </>
   );
 }
 
-function MessageDemo({ username,email }){
- 
-
+function MessageDemo({ username, email }) {
   return (
     <>
-    <h1>Hello {username} {email}</h1>
+      <h1>
+        Hello {username} {email}
+      </h1>
     </>
   );
 }
