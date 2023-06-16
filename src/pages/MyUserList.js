0dc045f0c590437f19de40a611c28554a1e9a06e
72464_Mayuri <mayuripatade08@gmail.com>
2023-06-16 14:59:36 +0530

    userlist

---
+++ src/pages/MyUserList.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+
+function MyUserList() {
+  const [userList, setUserList] = useState([]);
+
+  useEffect(() => {
+    getAllUserAction();
+  }, []);
+
+  const getAllUserAction = async () => {
+    let url = `http://localhost:4000/find-all-user`;
+    let res = await fetch(url);
+    let list = await res.json();
+
+    setUserList([...list]);
+  };
+
+  return (
+    <>
+      <h1>User List</h1>
+
+      {userList.map((item) => (
+        <>
+          <div>
+            {item.username} {item.password} {item.email} {item.mobile}
+          </div>
+        </>
+      ))}
+    </>
+  );
+}
+
+export default MyUserList;
