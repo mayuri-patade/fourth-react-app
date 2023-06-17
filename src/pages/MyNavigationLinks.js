942deacf0e9d079cc6f977a950d1f3ec743f7198
72464_Mayuri <mayuripatade08@gmail.com>
2023-06-17 18:37:06 +0530

    login

--- src/pages/MyNavigationLinks.js
+++ src/pages/MyNavigationLinks.js
@@ -1,7 +1,13 @@
 import { Container, Nav, NavDropdown, Navbar } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function MyNavigationLinks() {
+  let navigate = useNavigate();
+
+  const logOutAction = () => {
+    navigate("/login");
+  };
+
   return (
     <>
       <Navbar bg="dark" data-bs-theme="dark" expand="lg">
@@ -10,9 +16,13 @@ function MyNavigationLinks() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
+              <Nav.Link as={Link} to={"/login"}>
+                Login
+              </Nav.Link>
               <Nav.Link as={Link} to={"/home"}>
                 Home
               </Nav.Link>
+
               <Nav.Link as={Link} to={"/todo"}>
                 Todo
               </Nav.Link>
@@ -25,9 +35,11 @@ function MyNavigationLinks() {
               <Nav.Link as={Link} to={"/user-list"}>
                 User List
               </Nav.Link>
+
               <Nav.Link as={Link} to={"/validation-demo"}>
                 ValidationDemo
               </Nav.Link>
+              <Nav.Link onClick={logOutAction}>Log Out</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
